5cc7f53dde4ead670f3f8a515809a24efa0df130
unknown <liang-d@LIANG-D.PSDDN>
2016-02-24 11:35:10 +0800

    将decodePacket()方法从TcpTable中分离出去

---
+++ src/pcap/decode/Decode.java
@@ -0,0 +1,30 @@
+package pcap.decode;
+
+import org.jnetpcap.protocol.tcpip.Tcp;
+
+import pcap.record.TcpRecord;
+
+public class Decode {
+
+    /**
+     * 所有的decode都从这里调用
+     * */
+    public static void decodePacket(Tcp tcp, TcpRecord record, long timeStamp) {
+        if (null == tcp || null == record)
+            return;
+        String type = record.getType().toLowerCase();
+        if (0 == type.length()) {
+            return;
+        } else if (type.equals("http")) {
+            HttpDecode.decode(tcp, record, timeStamp);
+        } else if (type.equals("mysql")) {
+            MysqlDecode.decode(tcp, record, timeStamp);
+        } else if (type.equals("pgsql")) {
+        } else if (type.equals("mongodb")) {
+        } else if (type.equals("thrift")) {
+        } else if (type.equals("redis")) {
+        } else if (type.equals("ldap")) {
+        }
+    }
+
+}
